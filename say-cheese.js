8a5663f7a795e6ddbcafa3375fb5b6f38ec70f7c
leemachin <lee@new-bamboo.co.uk>
2012-08-04 21:29:37 +0100

    wrapper function for getting the userMedia stuff, and a bit more padding out

--- say-cheese.js
+++ say-cheese.js
@@ -2,7 +2,7 @@
  * Say Cheese!
  * Lee Machin, 2012
  * http://leemach.in, http://new-bamboo.co.uk
- * 
+ *
  * Minimal javascript library for integrating a webcam and snapshots into your app.
  *
  * Handles starting up the webcam and rendering the element, and also capturing shots
@@ -15,8 +15,9 @@
 
   var sayCheese;
 
+  /* Check for the existence of the userMedia feature. */
   function userMediaFeatureExists() {
-    return 'webkitGetUserMedia' in navigator ||  'getUserMedia' in navigator;
+    return 'getUserMedia' in navigator ||  'webkitGetUserMedia' in navigator;
   };
 
   sayCheese = function sayCheese() {
@@ -28,14 +29,37 @@
     }
   };
 
+  // the getUserMedia function is different on Webkit browsers, so we have to
+  // do a bit of faffing about to make sure we call the right one.
+  sayCheese.prototype.getUserMedia = function getUserMedia(success, error) {
+    userMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia);
+    userMedia.call({ video: true }, success, error).apply(this);
+  }
+
   /* The viewfinder is the element we use to preview the webcam stream */
-  sayCheese.prototype.viewfinder =  document.createElement('video');
+  sayCheese.prototype.createVideo = function() {
+    element = document.createElement('video');
+    element.autoplay = true;
+    return element;
+  }
 
   /* The canvas is used for taking 'photos' */
-  sayCheese.prototype.canvas = document.createElement('canvas');
+  sayCheese.prototype.createCanvas = function() {
+    element = document.createElement('canvas');
+    return element;
+  }
 
   sayCheese.prototype.start = function start() {
 
+    var success = function userMediaSuccess(stream) {
+      console.log('woop woop', stream);
+    }
+
+    var error = function userMediaError() {
+      console.log('nooooooooo');
+    }
+
+    this.getUserMedia(success, error);
   };
 
   sayCheese.prototype.stop = function stop() {
