7b3062abf0ff332244acedb27e5844347135c689
leemachin <lee@new-bamboo.co.uk>
2012-09-15 15:55:49 +0100

    add selective snapshot functionality (it works!)

--- say-cheese.js
+++ say-cheese.js
@@ -14,23 +14,31 @@
 
 var SayCheese = (function() {
 
-  var SayCheese,
-      viewfinder,
-      canvas,
-      context;
+  var SayCheese;
 
   /* Check for the existence of the userMedia feature. */
   function userMediaFeatureExists() {
     return 'getUserMedia' in navigator ||  'webkitGetUserMedia' in navigator;
   };
 
+  function eventCoords(evt) {
+     return { x: evt.offsetX || evt.layerX, y: evt.offsetY || evt.layerY };
+  };
+
   SayCheese = function SayCheese(element) {
     if (userMediaFeatureExists()) {
-      // do something
+
+      this.viewfinder = {},
+      this.snapshots = [],
+      this.canvas = null,
+      this.context = null,
+      this.video = null,
+      this.events = {};
+
       this.element = document.querySelectorAll(element)[0];
       this.element.style.position = 'relative';
 
-      this.readyEvent = new CustomEvent('saycheese:ready', true, false);
+      this.addEvent('ready', true, false);
     } else {
       // should make this more graceful in future
       throw new Error("getUserMedia() is not supported in this browser");
@@ -39,8 +47,26 @@ var SayCheese = (function() {
     return this;
   };
 
+  /*
+   * maintain a mapping of custom events
+   */
+  SayCheese.prototype.addEvent = function addEvent(name, bubble, cancel) {
+    this.events[name] = new CustomEvent('saycheese:'+name, bubble, cancel);
+  };
 
-  /* the getUserMedia function is different on Webkit browsers, so we have to
+  /*
+   * trigger one of the custom events
+   */
+  SayCheese.prototype.triggerEvent = function(name) {
+    if (this.events[name]) {
+      return document.dispatchEvent(this.events[name]);
+    }
+
+    return false;
+  };
+
+  /*
+   * the getUserMedia function is different on Webkit browsers, so we have to
    * do a bit of faffing about to make sure we call the right one.
    */
   SayCheese.prototype.getUserMedia = function getUserMedia(success, error) {
@@ -51,7 +77,8 @@ var SayCheese = (function() {
     })().call(this, { video: true }, success, error);
   };
 
-  /* Webkit behaves differently as regards getting a proper video URL. So we have
+  /*
+   * Webkit behaves differently as regards getting a proper video URL. So we have
    * to have another check for it.
    */
   SayCheese.prototype.getStreamUrl = function getStreamUrl(stream) {
@@ -62,7 +89,6 @@ var SayCheese = (function() {
     return (url && url.createObjectURL) ? url.createObjectURL(stream) : stream;
   };
 
-  /* The viewfinder is the element we use to preview the webcam stream */
   SayCheese.prototype.createVideo = function createVideo() {
     this.video = document.createElement('video');
     this.video.autoplay = true;
@@ -70,8 +96,8 @@ var SayCheese = (function() {
 
   SayCheese.prototype.setupCanvas = function setupCanvas() {
     this.canvas = document.createElement('canvas');
-    this.canvas.width = this.videoWidth = this.video.offsetWidth;
-    this.canvas.height = this.videoHeight = this.video.offsetHeight;
+    this.canvas.width = this.video.offsetWidth;
+    this.canvas.height = this.video.offsetHeight;
 
     this.canvas.style.position = 'absolute';
     this.canvas.style.top = this.video.offsetTop;
@@ -80,10 +106,10 @@ var SayCheese = (function() {
     this.context = this.canvas.getContext('2d');
 
     this.element.appendChild(this.canvas);
-    this.watermark();
+    this.initDynamicViewfinder();
 
     // we're now all set up, so dispatch the ready event
-    document.dispatchEvent(this.readyEvent);
+    this.triggerEvent('ready');
   };
 
   SayCheese.prototype.watermark = function watermark() {
@@ -96,16 +122,100 @@ var SayCheese = (function() {
     this.context.fillText('demo', x, y);
   };
 
-  /* The viewfinder is a region within the canvas that can be used to
-   * take snapshots of just part of the video stream.
-   */
-  SayCheese.prototype.viewfinder = function() {
 
+  /*
+   * The default viewfinder is just the exact size of the video
+   */
+  SayCheese.prototype.initDefaultViewfinder = function initDefaultViewfinder() {
+    this.viewfinder = {
+      startX: 0,
+      startY: 0,
+      width: this.video.offsetWidth,
+      height: this.video.offsetHeight
+    };
+  };
+
+  /*
+   *  This viewfinder can be resized to capture select portions of the video stream
+   */
+  SayCheese.prototype.initDynamicViewfinder = function initDynamicViewfinder() {
+    // track the dragging status when events are fired
+    var isDragging = false,
+        box = {};
+
+    var start = function start(evt) {
+      evt.preventDefault();
+      var coords = eventCoords(evt);
+      box.startX = coords.x;
+      box.startY = coords.y;
+      isDragging = true;
+    }.bind(this);
+
+    var stop = function stop(evt) {
+      evt.preventDefault();
+      isDragging = false;
+      this.viewfinder = box;
+      this.takeSnapshot();
+    }.bind(this);
+
+    var draw = function draw(evt) {
+      evt.preventDefault();
+      var coords = eventCoords(evt);
+      if (isDragging) {
+        box.width = coords.x - box.startX;
+        box.height = coords.y - box.startY;
+
+        // draw the shade
+        this.context.globalCompositeOperation = 'xor';
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.context.fillStyle = 'rgba(0, 0, 0, .8)';
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+        // draw the window
+        this.context.strokeStyle = 'rgba(255, 255, 255, .5)';
+        this.context.lineWidth = 2;
+        this.context.strokeRect(box.startX, box.startY, box.width, box.height);
+        this.context.fillRect(box.startX, box.startY, box.width, box.height);
+      }
+    }.bind(this);
+
+    this.canvas.addEventListener('mousedown', start, true);
+    this.canvas.addEventListener('mouseup', stop, true);
+    this.canvas.addEventListener('mousemove', draw, true);
+
+    // add touch events if they're there
+    if ('ontouchstart' in window) {
+      this.canvas.addEventListener('touchstart', start, true);
+      this.canvas.addEventListener('touchend', stop, true);
+      this.canvas.addEventListener('touchmove', draw, true);
+    }
+
+    // make the cursor a crosshair to make it more clear what can be done
+    this.canvas.style.cursor = 'crosshair';
   };
 
   /* Take a snapshot of the current state of the stream */
-  SayCheese.prototype.takeSnapshot = function takeSnapshot() {
-    this.context.drawImage(this.video, 0, 0, this.videoWidth, this.videoHeight);
+  SayCheese.prototype.takeSnapshot = function takeSnapshot(callback) {
+    var snapshot = document.createElement('canvas'),
+        ctx      = snapshot.getContext('2d');
+
+    snapshot.width = this.viewfinder.width,
+    snapshot.height = this.viewfinder.height;
+
+    ctx.drawImage(this.video,
+                       this.viewfinder.startX,
+                       this.viewfinder.startY,
+                       this.viewfinder.width,
+                       this.viewfinder.height,
+                       0,
+                       0,
+                       this.viewfinder.width,
+                       this.viewfinder.height);
+
+    this.snapshots.push(snapshot);
+
+    (callback || function() {}).call(this, snapshot);
+    ctx = null;
   };
 
   /* Start up the stream, if possible */
@@ -113,9 +223,9 @@ var SayCheese = (function() {
     var success = function success(stream) {
       this.createVideo();
 
-      // it'd be easier to do this inline, but we don't get the size of the video
-      // until the metadata's loaded :/
+      // video width and height don't exist until metadata is loaded
       this.video.addEventListener('loadedmetadata', this.setupCanvas.bind(this), false);
+      this.video.addEventListener('loadedmetadata', this.initDefaultViewfinder.bind(this), false);
 
       this.video.src = this.getStreamUrl(stream);
       this.element.appendChild(this.video);
@@ -135,7 +245,7 @@ var SayCheese = (function() {
 
   /* Stop it - TODO: figure out how to actually disable the stream */
   SayCheese.prototype.stop = function stop() {
-    console.log(video);
+    this.video = null;
     document.body.removeChild(video);
   };
 
