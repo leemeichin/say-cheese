44b92888a670c7a42c3b0a5e4a3370f4a75eccaa
Lee Machin <lee@new-bamboo.co.uk>
2014-03-02 18:28:59 +0000

    WIP: Tighten up the public interface.
    Move setup functions out of the prototype, mainly, and make it easier to
    throw in some sort of promises based solution for dealing with the audio
    and/or video setup in future.
    
    The end result should be something much smaller that only deals with
    starting up the webcam and audio streams, and makes it easier to plug in
    things that manipulate those things.

--- say-cheese.js
+++ say-cheese.js
@@ -17,142 +17,150 @@ var SayCheese = (function () {
   navigator.getUserMedia = (navigator.getUserMedia ||
                             navigator.webkitGetUserMedia ||
                             navigator.mozGetUserMedia ||
-                            navigator.msGetUserMedia ||
-                            false);
+                            navigator.msGetUserMedia)
 
   window.AudioContext = (window.AudioContext ||
-                         window.webkitAudioContext);
+                         window.webkitAudioContext)
 
   window.URL = (window.URL ||
-                window.webkitURL);
+                window.webkitURL)
 
-  return function SayCheese (element, options) {
-    this.video = null,
-    this.events = {},
-    this.stream = null,
-    this.options = {
-      audio: false
-    };
+  function slice (obj /*, ... */) {
+    var keys = Array.prototype.slice.call(arguments, 1)
 
-    this.setOptions(options);
-    this.element = document.querySelector(element);
-    return this;
-  };
+    return keys.reduce(function (memo, key) {
+      memo[key] = obj[key]
+      return memo
+    }, {})
+  }
 
-  SayCheese.prototype.on = function (evt, handler) {
-    if (this.events.hasOwnProperty(evt) === false) {
-      this.events[evt] = [];
-    }
-
-    this.events[evt].push(handler)
-  };
-
-  SayCheese.prototype.off = function (evt, handler) {
-    this.events[evt] = this.events[evt].filter(function(h) {
-      return h !== handler;
-    });
-  };
-
-  SayCheese.prototype.trigger = function (evt, data) {
-    if (this.events.hasOwnProperty(evt) === false) {
-      return;
-    }
-
-    this.events[evt].forEach(function (handler) {
-      handler.call(this, data);
-    }.bind(this));
-  };
-
-  SayCheese.prototype.setOptions = function (options) {
-    // just use naïve, shallow cloning
-    for (var opt in options) {
-      this.options[opt] = options[opt];
+  function setOptions (options, newOptions) {
+    for (var opt in newOptions) {
+      options[opt] = newOptions[opt];
     }
   }
 
-  SayCheese.prototype.getStreamUrl = function () {
+  function streamURL (stream) {
     if (window.URL && window.URL.createObjectURL) {
-      return window.URL.createObjectURL(this.stream);
-    } else {
-      return this.stream;
+      return window.URL.createObjectURL(stream)
     }
-  };
 
-  SayCheese.prototype.createVideo = function () {
-    var width     = 320,
-        height    = 0,
-        streaming = false;
+    return stream
+  }
 
-    this.video = document.createElement('video');
+  function initVideo (stream, onSuccess, onError) {
+    var video = document.createElement('video'),
+        width     = 320,
+        height    = 240,
+        streaming = false
 
-    this.video.addEventListener('canplay', function () {
+    video.addEventListener('canplay', function () {
       if (!streaming) {
-        height = this.video.videoHeight / (this.video.videoWidth / width);
-        this.video.style.width = width;
-        this.video.style.height = height;
-        streaming = true;
-        this.trigger('start');
+        height = video.videoHeight / (video.videoWidth / width)
+
+        video.style.width = width
+        video.style.height = height
+
+        streaming = true
+
+        onSuccess({ video: video })
       }
-    }.bind(this), false);
-  };
+    }, false)
 
-  SayCheese.prototype.linkAudio = function () {
-    this.audioCtx = new window.AudioContext();
-    this.audioStream = this.audioCtx.createMediaStreamSource(this.stream);
+    if (navigator.mozGetUserMedia) {
+        video.mozSrcObject = stream
+    } else {
+        video.src = streamURL(stream)
+    }
 
-    var biquadFilter = this.audioCtx.createBiquadFilter();
+    video.play()
 
-    this.audioStream.connect(biquadFilter);
-    biquadFilter.connect(this.audioCtx.destination);
-  };
+    return video
+  }
+
+  function initAudio (stream, onSuccess, onError) {
+    try {
+      var audioCtx = new window.AudioContext(),
+          audioStream = audioCtx.createMediaStreamSource(stream),
+          biquadFilter = audioCtx.createBiquadFilter(),
+          audioComponents = { context: audioCtx, stream: audioStream }
+
+      audioStream.connect(biquadFilter)
+      biquadFilter.connect(audioCtx.destination)
+
+      onSuccess(audioComponents)
+
+      return audioComponents
+
+    } catch(e) {
+      return onError(e)
+    }
+  }
+
+
+  return function SayCheese (element, options) {
+    this.video = null,
+    this.audio = null,
+    this.stream = null,
+    this.events = {},
+    this.options = setOptions({ audio: false, video: true }, options)
+
+    this.element = document.querySelector(element)
+
+    return this
+  }
+
+  SayCheese.prototype.on = function (evt, handler) {
+    if (this.events.hasOwnProperty(evt) === false) {
+      this.events[evt] = []
+    }
+
+    this.events[evt].push(handler)
+  }
+
+  SayCheese.prototype.off = function (evt, handler) {
+    this.events[evt] = this.events[evt].filter(function (h) {
+      return h !== handler
+    })
+  }
+
+  SayCheese.prototype.trigger = function (evt, data) {
+    if (this.events.hasOwnProperty(evt)) {
+      this.events[evt].forEach(function (handler) {
+        handler.call(this, data)
+      }.bind(this))
+    }
+  }
 
   /* Start up the stream, if possible */
   SayCheese.prototype.start = function () {
 
-    // fail fast and softly if browser not supported
-    if (navigator.getUserMedia === false) {
-      this.trigger('error', 'NOT_SUPPORTED');
-      return false;
-    }
+    var error = this.trigger.bind(this, 'error')
+    var start = this.trigger.bind(this, 'start')
 
     var success = function (stream) {
-      this.stream = stream;
-      this.createVideo();
+      this.stream = stream
+      this.video = initVideo(stream, start, error)
+      this.audio = initAudio(stream, start, error)
 
-      if (navigator.mozGetUserMedia) {
-        this.video.mozSrcObject = stream;
-      } else {
-        this.video.src = this.getStreamUrl();
-      }
+      this.element.appendChild(this.video)
 
-      if (this.options.audio === true) {
-        try {
-          this.linkAudio();
-        } catch(e) {
-          this.trigger('error', 'AUDIO_NOT_SUPPORTED');
-        }
-      }
+    }.bind(this)
 
-      this.element.appendChild(this.video);
-      this.video.play();
-    }.bind(this);
+    // fail fast and softly if browser not supported
+    if (!navigator.getUserMedia) return error('NOT_SUPPORTED');
 
-    /* error is also called when someone denies access */
-    var error = function (error) {
-      this.trigger('error', error);
-    }.bind(this);
-
-    return navigator.getUserMedia({ video: true, audio: this.options.audio }, success, error);
-  };
+    return navigator.getUserMedia(slice(this.options, 'video', 'audio'), success, error)
+  }
 
   SayCheese.prototype.stop = function () {
-    this.stream.stop();
+    this.stream.stop()
 
     if (window.URL && window.URL.revokeObjectURL) {
-      window.URL.revokeObjectURL(this.video.src);
+      window.URL.revokeObjectURL(this.video.src)
     }
 
-    return this.trigger('stop');
-  };
+    this.trigger('stop')
+  }
 
 })();
